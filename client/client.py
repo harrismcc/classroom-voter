dae05cb288b9f2b3e47f4d12bd7efe2750381a82
Harris McCullers <harrismcc@gmail.com>
2020-09-22 15:54:21 -0700

    added initial files and got started

---
+++ client/client.py
@@ -0,0 +1,32 @@
+import socket
+import os
+import sys
+
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("usage: python3 %s <host> <host-port>" % sys.argv[0])
+        quit(1)
+    host = sys.argv[1]
+    port = sys.argv[2]
+
+
+    clientSocket = socket.socket()
+
+    print('Waiting for connection')
+    try:
+        clientSocket.connect((host, int(port)))
+    except socket.error as e:
+        print(str(e))
+
+    while True:
+        msg = input('Say Something: ')
+        clientSocket.send(str.encode(msg))
+        response = clientSocket.recv(1024)
+        print(response.decode('utf-8'))
+
+    clientSocket.close()
+
+if __name__ == "__main__":
+    main()
---
+++ server/server.py
@@ -0,0 +1,55 @@
+import socket
+import os
+import sys
+from _thread import *
+
+
+
+def threaded_client(connection):
+    connection.send(str.encode('Welcome to the Poll\n'))
+
+    while True:
+        data = connection.recv(2048)
+
+
+        reply = 'Server Says: ' + data.decode('utf-8')
+        if not data:
+            break
+
+        connection.sendall(str.encode(reply))
+
+    #connection.close()
+
+def main():
+    if len(sys.argv) != 2:
+        print("usage: python3 %s <port>" % sys.argv[0])
+        quit(1)
+    port = sys.argv[1]
+
+    serverSocket = socket.socket()
+    host = '127.0.0.1'
+    port = int(port)
+    threadCount = 0
+
+    try:
+        serverSocket.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+
+    print('Waiting for a Connection..')
+    serverSocket.listen(5)
+
+    #continuiously accept new connections
+    while True:
+        client, address = serverSocket.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+
+        #each individual connection is threaded
+        start_new_thread(threaded_client, (client, ))
+        threadCount += 1
+        print('Thread Number: ' + str(threadCount))
+        serverSocket.close()
+
+
+if __name__ == "__main__":
+    main()
